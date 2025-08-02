a7eeaf4466aecd4d2f981cbc200144d23473712f
ali muhammad <alimarain55@gmail.com>
2025-08-03 00:27:08 +0500

    add categories in home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app/widgets/support_widgets.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,9 +10,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List categories = [
+    "images/headphone.jpg"
+        "images/laptop/jpg"
+        "images/tv.png"
+        "images/watch.jpg"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff2f2f2),
       body: Container(
         margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
         child: Column(
@@ -35,8 +44,73 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+                // padding: EdgeInsets.only(left: 20.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                width: MediaQuery.of(context).size.width,
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search Here",
+                      hintStyle: AppWidget.lightTextFieldStyle(),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      )),
+                )),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("categories", style: AppWidget.semiboldTextFieldStyle()),
+                Text("See All",
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold))
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                left: 20.0,
+              ),
+              height: 70,
+              child: ListView.builder(
+                  itemCount: categories.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return CategoryTile(image: categories[index]);
+                  }),
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  String image;
+  CategoryTile({required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      height: 90,
+      width: 90,
+      child: Column(
+        children: [
+          Image.asset(image),
+        ],
       ),
     );
   }
