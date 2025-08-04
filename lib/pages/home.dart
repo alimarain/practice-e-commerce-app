4ea72b0c3eef1c43efd41e11f9d7a63a5147fc6d
ali muhammad <alimarain55@gmail.com>
2025-08-04 23:37:03 +0500

    addition

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -77,19 +77,73 @@ class _HomeState extends State<Home> {
                         fontWeight: FontWeight.bold))
               ],
             ),
-            Container(
-                margin: EdgeInsets.only(
-                  left: 20.0,
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.only(right: 25.0),
+                  height: 130,
+                  decoration: BoxDecoration(
+                    color: Color(0xfffd6f3e),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "All",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
                 ),
-                height: 120,
-                child: ListView.builder(
-                  itemCount: categories.length,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return CategoryTile(image: categories[index]);
-                  },
-                ))
+                Expanded(
+                  child: Container(
+                      // margin: EdgeInsets.only(
+                      //   left: 20.0,
+                      // ),
+                      height: 130,
+                      child: ListView.builder(
+                        itemCount: categories.length,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return CategoryTile(image: categories[index]);
+                        },
+                      )),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              // padding: EdgeInsets.only(left: 10),
+              // margin: EdgeInsets.only(left: 25.0),
+              height: 190,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "images/tv.png",
+                          height: 60,
+                          width: 60,
+                          fit: BoxFit.cover,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -111,7 +165,7 @@ class CategoryTile extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Image.asset(
             image,
